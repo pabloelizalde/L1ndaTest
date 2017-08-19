37107b118506119a788ede95cf9819c2c45bb59e
Pablo Elizalde <pelizcar@gmail.com>
2017-08-19 18:37:37 +0200

    Added `data.json` and models

---
+++ L1ndaTest/Views/CelebrationModel.swift
@@ -0,0 +1,25 @@
+//
+//  CelebrationModel.swift
+//  L1ndaTest
+//
+//  Created by Pablo Elizalde Carvajal on 18/08/2017.
+//  Copyright © 2017 Pelizcar. All rights reserved.
+//
+
+import Foundation
+import Unbox
+
+struct CelebrationModel: Unboxable {
+	
+	let title: String
+	let colour: String
+	let rank: String
+	let rank_num: Double
+	
+	init(unboxer: Unboxer) throws {
+		title = try unboxer.unbox(key: "title")
+		colour = try unboxer.unbox(key: "colour")
+		rank = try unboxer.unbox(key: "rank")
+		rank_num = try unboxer.unbox(key: "rank_num")
+	}
+}
---
+++ L1ndaTest/Views/DayModel.swift
@@ -0,0 +1,27 @@
+//
+//  DayModel.swift
+//  L1ndaTest
+//
+//  Created by Pablo Elizalde Carvajal on 18/08/2017.
+//  Copyright © 2017 Pelizcar. All rights reserved.
+//
+
+import Foundation
+import Unbox
+
+struct DayModel: Unboxable {
+	
+	let date: String
+	let season: String
+	let season_week: Int
+	let celebrations: [CelebrationModel]
+	let weekday: String
+	
+	init(unboxer: Unboxer) throws {
+		date = try unboxer.unbox(key: "date")
+		season = try unboxer.unbox(key: "season")
+		season_week = try unboxer.unbox(key: "season_week")
+		celebrations = try unboxer.unbox(key: "celebrations")
+		weekday = try unboxer.unbox(key: "weekday")
+	}
+}
